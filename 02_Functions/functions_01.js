9848a54d9637f1c02b49ae91c1112770fdaa254a
Farooque Sajjad <22sw040@students.muet.edu.pk>
2024-10-04 06:49:45 -0700

    Create functions_01.js

---
+++ 02_Functions/functions_01.js
@@ -0,0 +1,79 @@
+"use strict";
+// Functions
+// function types
+// Optional and default parameters
+// Rest Parameters
+// Overloads
+function abcd0() {
+    return "Hello Ji";
+}
+function abcd1(name, cb) {
+}
+abcd1("Farooque", (value) => {
+    console.log(value);
+});
+function abcdef(name, age, cb) {
+    // cb((arg)=>{
+    // })
+    cb("Hello Ji");
+}
+abcdef("farooque", 21, (arg) => {
+    console.log("abcd");
+    for (let i = 0; i < 10; i++) {
+        console.log("Hello Ji ", i + 1);
+    }
+});
+// ***************************************************************************************
+// function abcdef2(name: string, age: number, gender?: string) {
+//     console.log("Name of Student :" + name);
+//     console.log("Name of Age     :" + age);
+//     console.log("Name of Gender  :" + gender);
+// }
+// abcdef2("Talha", 37, " Male");
+// abcdef2("Ada", 28, );
+// function abcdef2(name: string, age: number, gender: string="not to be disclosed") {
+//     console.log("Name of Student :" + name);
+//     console.log("Name of Age     :" + age);
+//     console.log("Name of Gender  :" + gender);
+// }
+// abcdef2("Talha", 37, "Male");
+// abcdef2("Ada", 28, );
+// *******************************************************************************
+// Rest Parameters
+// ... rest/spread
+// The rest operator in JavaScript (...) allows you to collect multiple elements into a single array. It is often used in function parameters to gather arguments or to combine array elements.
+function restPerameter(...args) {
+    args.forEach(element => {
+        console.log(element + "*");
+    });
+}
+restPerameter(1, 2, 3, 4, 5);
+function friends(...args) {
+    for (let i = 0; i < args.length + 2; i++) {
+        console.log(args[i]);
+    }
+}
+friends("Farooque", "Talha", "Ada", "H");
+// Spread Operator
+// The spread operator in JavaScript (...) allows you to expand an iterable (like an array or object) into individual elements. It's often used for copying arrays, combining objects, or passing elements as arguments to functions.
+var arr1 = [1, 2, 3, 4];
+var arr2 = [...arr1];
+var arr3 = [...arr1, 5, 6];
+console.log(arr1, arr2, arr3);
+// function abcd4(a: string, b: number) {
+//     if (typeof a === "string" && typeof b == "number") {
+//     }
+// }
+function abcd4(a, b) {
+    if (typeof a === "string" && b === undefined) {
+        console.log("Hello Ji");
+    }
+    else if (typeof a === "string" && typeof b === "number") {
+        return 12345;
+    }
+    else {
+        throw new Error("Something is bad.....");
+    }
+}
+abcd4("Hello Ji");
+console.log(abcd4("Hello Ji", 40));
