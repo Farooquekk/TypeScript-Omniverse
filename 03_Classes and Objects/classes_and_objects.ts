14122d51d6d6b6f9882ada82673dcd6db9ada5db
Farooque Sajjad <22sw040@students.muet.edu.pk>
2024-10-05 05:51:52 -0700

    Create classes_and_objects.ts

---
+++ 03_Classes and Objects/classes_and_objects.ts
@@ -0,0 +1,29 @@
+// Classes and Objects
+
+// class definition
+// constructors
+// access modifiers (public, private and protected)
+// read only properties
+// parameter properties
+// getters and setters
+// static members
+// abstract classes and methods
+
+class Device{
+    name = "Oppo";
+    price = 140000;
+    category = "digital";
+}
+
+let d1 = new Device();
+let d2 = new Device();
+
+// *******************************************
+
+class Bottle{
+    radius = 120;
+    price = 100;
+    color = "white";
+}
+
+let b1 = new Bottle();
