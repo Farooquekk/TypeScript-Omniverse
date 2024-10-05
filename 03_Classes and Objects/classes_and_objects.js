675cb532cc79ae3746c85aea3e497594ff248844
Farooque Sajjad <22sw040@students.muet.edu.pk>
2024-10-05 05:52:38 -0700

    Create classes_and_objects.js

---
+++ 03_Classes and Objects/classes_and_objects.js
@@ -0,0 +1,28 @@
+"use strict";
+// Classes and Objects
+// class definition
+// constructors
+// access modifiers (public, private and protected)
+// read only properties
+// parameter properties
+// getters and setters
+// static members
+// abstract classes and methods
+class Device {
+    constructor() {
+        this.name = "Oppo";
+        this.price = 140000;
+        this.category = "digital";
+    }
+}
+let d1 = new Device();
+let d2 = new Device();
+// *******************************************
+class Bottle {
+    constructor() {
+        this.radius = 120;
+        this.price = 100;
+        this.color = "white";
+    }
+}
+let b1 = new Bottle();
