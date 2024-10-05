1a4ce031e490f938fc3e968d64ad86522cc80ba3
Farooque Sajjad <22sw040@students.muet.edu.pk>
2024-10-05 05:57:09 -0700

    Create access_modifiers.js

---
+++ 05_Access Modifiers/access_modifiers.js
@@ -0,0 +1,49 @@
+"use strict";
+// public: Members are accessible from anywhere, both inside and outside the class. This is the default modifier if none is specified.
+// private: Members are only accessible within the class they are declared in. They cannot be accessed from outside the class or from derived classes.
+// protected: Members are accessible within the class and its subclasses, but not from outside the class hierarchy.
+// class BottleMaker1{
+//     constructor(private name: string) {
+//         this.name = name;
+//     }
+//     changing() {
+//         this.name = "Farooque"
+//     }
+// }
+// let bot1 = new BottleMaker1("Milton");
+// bot1.name = "Pata nahi";
+// **************************************************************
+// class BottleMaker2{
+//     private pataNahi: string = "Really Nahi Pata";
+//     constructor(public name:string){}
+// }
+// class MetalBottleMaker extends BottleMaker2{
+//     constructor(name: string) {
+//         super(name);
+//     }
+//     getValue() {
+//         console.log(this.name , this.pataNahi); // pata nahi will cause error but run in chrome developer tool
+//     }
+// }
+// let m1 = new MetalBottleMaker("Metal");
+// m1.getValue();
+// **************************************************************************
+// Access Modifier --> Protected
+class BottleMaker3 {
+    constructor() {
+        this.name = "BOTTA";
+        // constructor(){}
+    }
+}
+class MetalBottleMaker3 extends BottleMaker3 {
+    constructor() {
+        super(...arguments);
+        this.material = "metal";
+    }
+    change() {
+        this.name = "Change Nahi Horaha ha ";
+    }
+}
+let mb1 = new MetalBottleMaker3();
+mb1.change();
+//mb1.name = "Bahir se change kar rha hun ";
