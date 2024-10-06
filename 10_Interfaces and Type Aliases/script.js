7f51d2c7f57309b30cf1bdd98ebf945b9d61355c
Farooque Sajjad <22sw040@students.muet.edu.pk>
2024-10-06 04:58:27 -0700

    Create script.js

---
+++ 10_Interfaces and Type Aliases/script.js
@@ -0,0 +1,31 @@
+"use strict";
+// Interfaces and Type Aliases
+// Defining Interfaces
+// Using Interfaces to Define Obj Shapes
+// Extending Interfaces
+// Type Aliases
+// Intersection Types
+function abc(a, b) {
+    // a. // it will give you all functions of number
+    // b. // it will give you all functions of string
+}
+function getDataOfUser(obj) {
+}
+getDataOfUser({ name: "Farooque", email: "farooquekk92@gmail.com", password: "123", gender: "male" });
+function abcd2(obj) {
+}
+abcd2({ name: "Farooque", email: "farooquekk92@gmail.com", password: "123", admin: true });
+function abcd3(obj) {
+    obj.age = 14;
+    obj.name = "Jonny";
+}
+// ****************************************************************************************
+// Type Aliases
+let a2;
+let Ada;
+let vars;
+function abc1(obj) {
+}
+abc1("KK");
+// Interface:- supports declaration merging and is ideal for defining object shapes.  
+// Type:- is more flexible, used for primitives, unions, and complex type compositions.
