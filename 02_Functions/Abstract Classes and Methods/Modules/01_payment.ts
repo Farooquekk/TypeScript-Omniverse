31a32bbff8c2f4c5c68066098a5b87e306d7e74a
Farooque Sajjad <22sw040@students.muet.edu.pk>
2024-10-07 04:52:27 -0700

    Create 01_payment.ts

---
+++ 02_Functions/Abstract Classes and Methods/Modules/01_payment.ts
@@ -0,0 +1,14 @@
+
+// exporting function
+export function addPayment(p: number) {
+    console.log(p);
+    
+ }
+
+export function getDetails() { }
+
+
+// exporting class
+export default class Payment{  // need to add export default for exporting class
+    constructor(public payment:number){}
+}
