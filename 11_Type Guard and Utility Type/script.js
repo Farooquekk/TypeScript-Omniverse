59a8222a78ccb9a0806b3004b05462041f9ae6c7
Farooque Sajjad <22sw040@students.muet.edu.pk>
2024-10-09 04:50:30 -0700

    Create script.js

---
+++ 11_Type Guard and Utility Type/script.js
@@ -0,0 +1,40 @@
+"use strict";
+// Type Guards and Typescript utility Types
+// Using typeof and instanceof
+// Partial, Required, and ReadOnly
+// type guards --> type narrowing
+function guards(args) {
+    if (typeof args === "number") {
+        return "number";
+    }
+    else if (typeof args === "string") {
+        return "string";
+    }
+    else {
+        throw new Error("Only accept string or number");
+    }
+}
+console.log(guards(10));
+console.log(guards("Hello"));
+guards(true);
+// typeof instanceof --> instance narrowing
+class TvKaRemote {
+    switchTvOff() {
+        console.log("Switching off the TV...");
+    }
+}
+class CarKaRemote {
+    switchCarOff() {
+        console.log("Switching off the CAR...");
+    }
+}
+const tv = new TvKaRemote();
+const car = new CarKaRemote();
+function switchOffKaro(device) {
+    if (device instanceof TvKaRemote) {
+        device.switchTvOff();
+    }
+    else if (device instanceof CarKaRemote) {
+        device.switchCarOff();
+    }
+}
