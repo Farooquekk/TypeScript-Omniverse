e1f8b0663a0060e92383e77a0c77bcb52f3dfc57
Farooque Sajjad <22sw040@students.muet.edu.pk>
2024-10-09 04:51:40 -0700

    Create script.ts

---
+++ 12_Type Assertion and Casting/script.ts
@@ -0,0 +1,35 @@
+// Type Assertion
+// Type Casting
+// Non-null assertion operator
+
+
+// Type Assertion in TypeScript is a way to tell the compiler, "I know the type of this value better than you do." It lets you treat a value as a specific type when TypeScript can't automatically figure it out.
+
+let a0: any = 12;
+
+// a0.  // will not show me any intelliscence
+(a0 as number).toExponential;
+(<string>a0).length;
+
+// ****************************************************************************************
+
+// Type Casting
+
+let n1 = Number("23");
+console.log(n1);
+console.log(typeof n1);
+
+// ****************************************************************************************
+
+// Non-null assertion operator
+
+let oper: null | undefined|string;
+
+oper = "12";
+oper!.substring // oper is noe not null and undefined
+
+
+
+
+
+
