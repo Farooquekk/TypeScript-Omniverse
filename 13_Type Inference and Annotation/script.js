e4beef85c852232effadcaea5267864f492b55c2
Farooque Sajjad <22sw040@students.muet.edu.pk>
2024-10-09 04:54:18 -0700

    Create script.js

---
+++ 13_Type Inference and Annotation/script.js
@@ -0,0 +1,17 @@
+"use strict";
+// Type Inference
+// Understanding Type Inference
+// Type Annotations
+// Inference
+// Inference in TypeScript is automatically determining variable types from their values.
+let inference = 12;
+// Annotation
+// In TypeScript, annotation specifies types for variables or functions.
+let annotation;
+annotation = "Farooque";
+annotation = true;
+annotation = 40;
+// ****************************************************************************************
+let a1;
+function abcd(a, b) {
+}
